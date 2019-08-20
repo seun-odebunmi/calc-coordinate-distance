a17303858405fb66f59f68380e2fbd9e4c8cd1f5
Oluwaseun Odebunmi <Oluwaseun.Odebunmi@Interswitchng.com>
2019-08-20 18:26:02 +0100

    added more test cases

--- tests/Calculator.test.ts
+++ tests/Calculator.test.ts
@@ -5,17 +5,34 @@ describe('calculateDistance', function() {
     let result = Calculator.GetDistance([1, 4], [4, 4]);
     expect(result).toBe(1.0606601717798212);
   });
+  it('distance', function() {
+    let result = Calculator.GetDistance([4, 4], [3, 2]);
+    expect(result).toBe(2.449489742783178);
+  });
+  it('distance', function() {
+    let result = Calculator.GetDistance([3, 2], [5, 1]);
+    expect(result).toBe(1.5);
+  });
 });
 
-describe('calculatePoints', function() {
-  let coord = [[1, 4], [4, 4], [3, 2], [5, 1]];
-  it('closest', function() {
-    let result = Calculator.GetClosestPoints(coord);
+describe('calculateClosestPoints', function() {
+  it('fourCoordinates', function() {
+    let result = Calculator.GetClosestPoints([[1, 4], [4, 4], [3, 2], [5, 1]]);
     expect(result).toStrictEqual([[1, 4], [4, 4]]);
   });
+  it('threeCoordinates', function() {
+    let result = Calculator.GetClosestPoints([[1, 4], [4, 4], [3, 2]]);
+    expect(result).toStrictEqual([[1, 4], [4, 4]]);
+  });
+});
 
-  it('farthest', function() {
-    let result = Calculator.GetFarthestPoints(coord);
+describe('calculateFarthestPoints', function() {
+  it('fourCoordinates', function() {
+    let result = Calculator.GetFarthestPoints([[1, 4], [4, 4], [3, 2], [5, 1]]);
+    expect(result).toStrictEqual([[4, 4], [3, 2]]);
+  });
+  it('threeCoordinates', function() {
+    let result = Calculator.GetFarthestPoints([[1, 4], [4, 4], [3, 2]]);
     expect(result).toStrictEqual([[4, 4], [3, 2]]);
   });
 });
